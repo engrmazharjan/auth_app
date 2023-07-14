75997a123f6fc3afadda0ef2d863bab09ba50501
engrmazharjan <mazharj4565@gmail.com>
2023-07-14 15:52:13 +0500

    Installed react-router-dom And Create Different Routes In The App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,57 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+// * Import All Components
+import Username from "./components/Username";
+import Reset from "./components/Reset";
+import Register from "./components/Register";
+import Recovery from "./components/Recovery";
+import Profile from "./components/Profile";
+import Password from "./components/Password";
+import PageNotFound from "./components/PageNotFound";
+
+// * Root Routes
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Username></Username>,
+  },
+
+  {
+    path: "/register",
+    element: <Register></Register>,
+  },
+
+  {
+    path: "/password",
+    element: <Password></Password>,
+  },
+
+  {
+    path: "/profile",
+    element: <Profile></Profile>,
+  },
+
+  {
+    path: "/recovery",
+    element: <Recovery></Recovery>,
+  },
+
+  {
+    path: "/reset",
+    element: <Reset></Reset>,
+  },
+
+  {
+    path: "*",
+    element: <PageNotFound></PageNotFound>,
+  },
+]);
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">MERN LOGIN APP</h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 };
 
