e320120a7b6bc695099e6377b1f2ab47689095e3
engrmazharjan <mazharj4565@gmail.com>
2023-07-18 11:35:40 +0500

    Setup MongoDB Memory Server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import connect from "./database/connection.js";
 
 const app = express();
 
-// * Middlewares
+/* Middlewares */
 app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
@@ -12,12 +13,22 @@ app.disable("x-powered-by"); // Less hackers know about your stack
 
 const port = 8080;
 
-// * HTTP GET Request
+/* HTTP GET Request */
 app.get("/", (req, res) => {
   res.send("Hello World! / Home GET Request");
 });
 
-// * Start Server
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+/* Start Server Only When We Have Valid Connection */
+connect()
+  .then(() => {
+    try {
+      app.listen(port, () => {
+        console.log(`Server running on http://localhost:${port}`);
+      });
+    } catch (error) {
+      console.log("Cannot connect to the server");
+    }
+  })
+  .catch((error) => {
+    console.log("Invalid database connection");
+  });
